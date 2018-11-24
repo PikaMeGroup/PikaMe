cf794ab090de855eeffe44af35eac5ceb03ada93
rnorfield <rnorfield@gmail.com>
2018-11-23 19:03:27 -0800

    css + js index page

--- public/javascript/poke.js
+++ public/javascript/poke.js
@@ -1,39 +1,10 @@
-// console.log('hi');
-// function pokeSubmit(){
-//     // var param = document.getElementById("pokeInput").value;
-//     var pokeURL = "http://pokeapi.co/api/v1/pokemon/" + 23;
-
-//     $.getJSON(pokeURL, function(data){
-//         //console.log(data);
-//         //console.log(JSON.stringify(data, null, "  "));
-
-// /*********** New Stuff ****************************/
-//         var pokeID = data.national_id;
-//         var pokeName = data.name;
-//         var pokeType1 = data.types[0].name;
-//         if (data.types.length == 2) {
-//             var pokeType2 = data.types[1].name;
-//         }
-//         else var pokeType2 = null;
-
-//         console.log("Number: ", pokeID);
-//         console.log("Name: ", pokeName);
-//         console.log("Type 1: ", pokeType1);
-//         console.log("Type 2: ", pokeType2);
-// /*********** New Stuff ****************************/
-// });
-// };
-// pokeSubmit();
-
-
-document.getElementById('pokeBut').addEventListener('click', findPoke);
-
+document.getElementById('pokeBut').addEventListener('click', displayAllGroup);
 var myData;
 
 function getData(link) {
     
     return ($.ajax({
-        url: `https://pokeapi.co/api/v2/` + link,
+        url: `http://pokeapi.co/api/v2/` + link,
         dataType: 'json',
         async: false,
         success: function(data) {
@@ -43,12 +14,44 @@ function getData(link) {
 }
 
 function findPoke() {
-    var num = document.getElementById('pokeInput').value
-    var pokemonLink = 'pokemon/' + num
+    var pokemon = document.getElementById('pokeInput').value
+    var pokemonLink = `pokemon/` + pokemon
     pokemonData = getData(pokemonLink)
     console.log(pokemonData)
     var name = pokemonData.name
     var image = pokemonData.sprites.front_default
     document.getElementById("pokeName").innerHTML = name;
     document.getElementById("pokePic").src = image;
+    displayAllGroup()
+}
+
+function displayAllGroup() {
+    console.log("displaygroup");
+    var data = getData('egg-group/1/');
+    var group = data.pokemon_species;
+    for (var i = 0;  i < group.length - 1; i++) {
+        displayPokemon(group[i]);
+    }
+}
+
+function displayPokemon(pokemon) {
+    console.log("displayPokemon");
+    console.log(pokemon);
+    var data = getData('pokemon/' + pokemon.name);
+    console.log(data);
+    var name = data.name;
+    var formatted_name =  name.charAt(0).toUpperCase() + name.slice(1);
+    var sprite = data.sprites.front_default;
+    var div = document.createElement("DIV");
+    div.setAttribute("id", "pokeDisplay");
+    var para = document.createElement("p");
+    var text = document.createTextNode(formatted_name);
+    para.appendChild(text);
+    para.setAttribute("id", "pokePara");
+    var img = document.createElement("img");
+    img.setAttribute("id", "pokeImg");
+    img.setAttribute("src", sprite);
+    div.appendChild(img);
+    div.appendChild(para);
+    document.getElementById("display").appendChild(div);
 }
