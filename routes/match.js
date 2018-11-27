a74239dec08f6a66873cb4260e34b9db82221355
yuyulai22 <allisonlaiuu@gmail.com>
2018-11-27 14:10:20 -0800

    deleted function in matchjs

--- routes/match.js
+++ routes/match.js
@@ -2,17 +2,3 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 
-
-router.get('/userList', function(req,res) {
-	console.log('getting all users...')
-	User.find({}, function(err, users) {
-    var userMap = {};
-
-    users.forEach(function(user) {
-      userMap[user._id] = user;
-    });
-
-    res.send(userMap); 
-    console.log('done getting all users...')
-
-})
