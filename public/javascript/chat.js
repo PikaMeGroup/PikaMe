57917aca699b434c22f4c236a60ead0f916e7de1
yuyulai22 <allisonlaiuu@gmail.com>
2018-11-27 14:06:44 -0800

    get all users from db

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -1,3 +1,11 @@
 document.getElementById('chatbox').addEventListener('click', function(){
 	console.log('chatbox has been clicked');
+	$.ajax({
+	  type: "GET",
+	  url: "/userList",
+	success: function(res) {
+        console.log(res);
+        alert(res);
+    }});
+
 })
