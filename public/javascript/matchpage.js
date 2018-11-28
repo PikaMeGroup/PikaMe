0cfa3cf28690966e76c164e18d2642ad5b361ab5
rnorfield <rnorfield@gmail.com>
2018-11-27 16:43:06 -0800

    Match page changes
    some visuals changes

--- public/javascript/matchpage.js
+++ public/javascript/matchpage.js
@@ -10,3 +10,14 @@ function getData(link) {
     })).responseJSON;
 }
 
+document.getElementById('matchbtn').addEventListener('click', function(){
+	console.log('matchbtn has been clicked');
+	$.ajax({
+	  type: "GET",
+	  url: "/userList",
+	success: function(res) {
+        console.log("res is", res);
+        // alert(res);
+    }});
+
+})
