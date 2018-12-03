96d9bfa11e940616b5b1fa0dc646455bee5d6b7e
yuyulai22 <allisonlaiuu@gmail.com>
2018-12-02 18:21:48 -0800

    fixed mongo import

--- app.js
+++ app.js
@@ -37,6 +37,22 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
+// connecting to mongo atlas
+const MongoClient = require('mongodb').MongoClient;
+
+// replace the uri string with your connection string.
+const uri = "mongodb+srv://clusterpika-5roqm.mongodb.net/test"
+MongoClient.connect(uri, function(err, client) {
+   if(err) {
+        console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+   }
+   console.log('Connected...');
+   const collection = client.db("starwars").collection("star");
+   // perform actions on the collection object
+   client.close();
+});
+
+
 
 app.use(express.static(__dirname + '/node_modules'));
 
