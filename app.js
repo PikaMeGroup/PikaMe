1409d98de827fbe8dc8e90bd0b85b2bb51db9336
rnorfield <rnorfield@gmail.com>
2018-12-02 17:47:17 -0800

    appjs port

--- app.js
+++ app.js
@@ -142,6 +142,9 @@ io.on('connection', (socket) => {
 	})
 })
 
+const port = process.env.PORT || 3000;
 
-server.listen(3000)
+server.listen(port, () => {
+	console.log(`Server is up on port ${port}`);
+})
 module.exports = app;
